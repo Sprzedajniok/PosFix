1b879bc8bca8baf99610067d2a6953d38d9d7c13
Sprzedajniok <maxxxi06@gmail.com>
2022-05-29 22:29:22 +0200

    FIRST CRITICAL ERROR POGGERS
    JESUS TAKE THE WHEEL

--- Plugin.cs
+++ Plugin.cs
@@ -14,7 +14,7 @@ namespace PosFix
     {
         internal static Plugin Instance { get; set; }
         internal static IPALogger Log { get; set; }
-        private OVRResetOrientation _resetPos;
+        public OVRResetOrientation _resetPos;
 
         [Init]
         public void Init(Config config, IPALogger logger)
@@ -35,12 +35,30 @@ namespace PosFix
             _resetPos.resetButton = OVRInput.RawButton.X;
             GameObject.DontDestroyOnLoad(_resetPos);
             Log.Info("Posistion reset on startup");
-            //is this even good? Anything happens?
-            PosFix.Setup();
+            //Okay so this little piece of shit throws some errors?
+            // -->   PosFix.Setup();
+            // BS_Utils] Caught Exception when executing event: Exception has been thrown by the target of an invocation.
+            // [ERROR @ 22:18:22 | BS_Utils]  In Assembly: PosFix, Version = 0.0.1.0, Culture = neutral, PublicKeyToken = null
+            //
             Log.Info("dupa");
         }
 
-            [OnExit]
+        //Dunno, just copied that ovrrresetorientation 
+        private void Update()
+        {
+            if (OVRInput.GetDown(_resetPos.resetButton))
+            {
+                OVRManager.display.RecenterPose();
+                Log.Info("test");
+                //FIRST CRITICAL ERROR POGGERS
+                //BUT AT LEAST I KNOW SOMETHING HAPPENS WHEN BUTTON IS PRESSED WOHOOO
+                // [CRITICAL @ 22:27:26 | UnityEngine] NullReferenceException: Object reference not set to an instance of an object
+                // [CRITICAL @ 22:27:26 | UnityEngine] OVRResetOrientation.Update()(at < 56fd09ffbe204ba5b452536e1c7b2566 >:0)
+
+            }
+        }
+
+        [OnExit]
         public void OnApplicationQuit()
         {
             //Same stuff as before ffs
@@ -51,3 +69,4 @@ namespace PosFix
         }
     }
 }
+
