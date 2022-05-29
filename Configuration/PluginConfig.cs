857e9cfb131ad51ff4e0ff08ecf3950a7034892e
Sprzedajniok <maxxxi06@gmail.com>
2022-05-29 02:02:27 +0200

    Initial commit

---
+++ Configuration/PluginConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+using IPA.Config.Stores;
+
+[assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
+namespace PosFix.Configuration
+{
+    internal class PluginConfig
+    {
+        public static PluginConfig Instance { get; set; }
+        public virtual bool EnableChecks { get; set; } = true;
+        public virtual bool EnableFPFC { get; set; } = true;
+
+        public virtual void OnReload()
+        {
+            /**/
+        }
+
+        public virtual void Changed()
+        {
+            /**/
+        }
+
+        public virtual void CopyFrom(PluginConfig other)
+        {
+            /**/
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,63 @@
+﻿using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using PosFix.Configuration;
+using BeatSaberMarkupLanguage.Settings;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+using IPALogger = IPA.Logging.Logger;
+
+namespace PosFix
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
+        public void Init(Config config, IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            Log.Info("PosFix initialized.");
+
+            PluginConfig.Instance = config.Generated<PluginConfig>();
+            BSMLSettings.instance.AddSettingsMenu("PosFix", "PosFix.Views.Settings.bsml", Configuration.PluginConfig.Instance);
+        }
+
+        #region BSIPA Config
+        //Uncomment to use BSIPA's config
+        /*
+        [Init]
+        public void InitWithConfig(Config conf)
+        {
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            Log.Debug("Config loaded");
+        }
+        */
+        #endregion
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+
+        }
+    }
+}
